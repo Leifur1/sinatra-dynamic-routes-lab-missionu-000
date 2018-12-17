1360857757497f6dec54e4c3f93f5119a963bde9
Leifur Sigurdsson <leifurerik@gmail.com>
2018-12-17 17:29:00 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -23,7 +23,14 @@ class App < Sinatra::Base
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}." 
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
 
+  get /:operation/:number1/:number2 do
+    @num1 = params[:number1].to_i
+    @num2 = params[:number2].to_i
+    @operator = params[:operation]
+    @sum = (@num1 @operator @num2)
+    "#{@sum}"
+  end
 end
