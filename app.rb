10a955b197b1a244325b0508968d1dbae2b0d996
Leifur Sigurdsson <leifurerik@gmail.com>
2018-12-17 17:36:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,10 +1,8 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
   get '/reversename/:name' do
-    @name = params[:name].reverse
-    "#{@name}"
+    params[:name].reverse
   end
 
   get '/square/:number' do
@@ -12,22 +10,34 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
-    @number = params[:number].to_i
-    @phrase = params[:phrase]
-    @number.times do |i|
-      print i, "#{@phrase}"
+    answer = ''
+
+    params[:number].to_i.times do
+      answer += params[:phrase]
     end
+
+    answer
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
     "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
 
-  # get /:operation/:number1/:number2 do
-  #   @num1 = params[:number1].to_i
-  #   @num2 = params[:number2].to_i
-  #   @operator = params[:operation]
-  #   @sum = @num1 @operator @num2
-  #   "#{@sum}"
-  # end
+  get '/:operation/:number1/:number2' do
+    number1 = params[:number1].to_i
+    number2 = params[:number2].to_i
+
+    answer = 'Unable to perform this operation'
+
+    case params[:operation]
+    when 'add'
+      answer = (number1 + number2).to_s
+    when 'subtract'
+      answer = (number1 - number2).to_s
+    when 'multiply'
+      answer = (number1 * number2).to_s
+    when 'divide'
+      answer = (number1 / number2).to_s
+    end
+  end
 end
